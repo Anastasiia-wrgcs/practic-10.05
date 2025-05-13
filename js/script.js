a3deb5d1909290669aeefc3252b6093520d6400b
3-126-5-PC <you@example.com>
2025-05-13 12:53:25 +1200

    13.05.25

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const HOST = 'http://api-messenger.web-srv.local';
-const CONTENT = document.querySelector('.content')
+const CONTENT = document.querySelector('.content');
+var TOKEN = ""
+
 
 function _get(params, callback) {
     var HTTP_REQUEST = new XMLHttpRequest();
@@ -7,7 +9,41 @@ function _get(params, callback) {
     HTTP_REQUEST.send();
     HTTP_REQUEST.onreadystatechange = function() {
         if(HTTP_REQUEST.readyState == 4) {
-            callback(HTTP_REQUEST.responseText)
+            callback(HTTP_REQUEST.responseText);
         }
     }
+}
+
+function _post (params, callback) {
+    var HTTP_REQUEST = new XMLHttpRequest();
+    HTTP_REQUEST.open('POST', `${params.url}`);
+    HTTP_REQUEST.send(params.data);
+    HTTP_REQUEST.onreadystatechange = function() {
+        if(HTTP_REQUEST.readyState == 4) {
+            callback(HTTP_REQUEST.responseText);
+        }
+    }
+}
+LoadPageAuth()
+
+function LoadPageAuth () {
+    _get ({url: '/modules/authorization.html'}, function(responseText) {
+        CONTENT.innerHTML = responseText
+    })
+}
+onLoadPageAuth()
+
+function onLoadPageAuth() {
+    document.querySelector('.authorize').addEventListener('click', function() {
+        var request_data = new FormData();
+        request_data.append('fam', document.querySelector('input[name="FAM"]').value)
+        request_data.append('ima', document.querySelector('input[name="ima"]').value)
+        request_data.append('E-mail', document.querySelector('input[name="E-mail"]').value)
+        request_data.append('parol', document.querySelector('input[name="parol"]').value)
+        
+
+        
+
+
+    })
 }

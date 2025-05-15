322fe8ffb72af56d03395ef28bd608c4a6d2d3b9
3-126-5-PC <you@example.com>
2025-05-15 17:54:40 +1200

    15.05.25

--- js/script.js
+++ js/script.js
@@ -24,24 +24,33 @@ function _post (params, callback) {
         }
     }
 }
-LoadPageAuth()
 
-
-function LoadPageAuth () {
-    _get ({url: 'modules/registration.html'}, function(responseText) {
+    _post ({url: 'modules/registration.html'}, function(responseText) {
         CONTENT.innerHTML = responseText
     })
-}
-onLoadPageAuth()
+        LoadPageAuth()
+        LoadPageChat()
 
 
-function onLoadPageAuth() {
+function LoadPageAuth() {
     document.querySelector('.registr').addEventListener('click', function() {
         var request_data = new FormData();
         request_data.append('fam', document.querySelector('input[name="FAM"]').value)
         request_data.append('ima', document.querySelector('input[name="ima"]').value)
         request_data.append('E-mail', document.querySelector('input[name="E-mail"]').value)
         request_data.append('parol', document.querySelector('input[name="parol"]').value)
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', `${HOST}/USER/`);
+        xhr.send(request_data);
+        xhr.onreadystatechange = function() {
+            if (xhr.status == 200) {
+                OnLoadPageChat()
+            } if (xhr.status == 422) {
+                var response = JSON.parse(xhr.responseText)
+                alert(response.message)
+            }
+        }
     })
 }
 
